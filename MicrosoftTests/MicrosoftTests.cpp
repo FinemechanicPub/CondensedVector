d1958ca62c788d2fb315393d232c11b982a1f9ff
Alexander Rubtsov <equester@mail.ru>
2023-08-30 17:52:06 +0300

    Tests for inserting and deleting elements

--- MicrosoftTests/MicrosoftTests.cpp
+++ MicrosoftTests/MicrosoftTests.cpp
@@ -31,6 +31,17 @@ namespace MicrosoftTests
 		return message + L" for test case " + vector_to_string(testcase);
 	}
 
+	std::wstring message_for_testcase(
+		const std::wstring& message,
+		const std::vector<int>& testcase,
+		const std::vector<int>& expected,
+		const std::vector<int>& actual) 
+	{
+		return message + L" Test case: " + vector_to_string(testcase)
+			+  L" Expected: " + vector_to_string(expected) 
+			+ L", Actual: " + vector_to_string(actual);
+	}
+
 	TEST_CLASS(MicrosoftTests)
 	{
 	public:
@@ -73,6 +84,50 @@ namespace MicrosoftTests
 				}
 			}
 
+		}
+		TEST_METHOD(InsertRemoveElements) {
+			auto is_non_empty = [](const int& x) {
+				return x != 0;
+			};
+			std::vector<std::vector<int>> testcases = {
+				{0, 0, 0, 0, 0, 0, 0},
+				{0, 5, 5, 5, 5, 5, 5},
+				{5, 5, 5, 5, 5, 5, 0},
+				{5, 5, 5, 5, 5, 5, 5},
+				{1, 3, 5, 7, 9, 11, 13},
+				{0, 1, 3, 5, 7, 9, 11, 13},
+				{1, 0, 3, 5, 7, 9, 11, 13},
+				{1, 3, 5, 7, 9, 11, 0, 13},
+				{1, 3, 5, 7, 9, 11, 13, 0},
+				{1, 3, 5, 0, 0, 7, 9, 11, 13},
+				{0, 0, 6, 8, 0, 3, 5, 0}
+			};
+			for (auto v : testcases) {
+				CondensedVector<int> cv(v, is_non_empty);
+				cv.Insert(0, 5);
+				v.insert(v.begin(), 5, 0);
+				Assert::IsTrue(v == cv.ToVector(v.size()), L"Insert to beginning error");
+			}
+			for (auto v : testcases) {
+				CondensedVector<int> cv(v, is_non_empty);
+				cv.Insert(v.size(), 5);
+				v.insert(v.end(), 5, 0);
+				Assert::IsTrue(v == cv.ToVector(v.size()), L"Append to end error");
+			}
+			for (const auto testcase : testcases) {
+				auto v = testcase;
+				CondensedVector<int> cv(v, is_non_empty);
+				cv.Delete(0, 5);
+				v.erase(v.begin(), v.begin() + 5);
+				Assert::IsTrue(v == cv.ToVector(v.size()), message_for_testcase(std::wstring(L"Delete from the beginning error."), testcase, v, cv.ToVector(v.size())).c_str());
+			}
+			for (const auto& testcase : testcases) {
+				auto v = testcase;
+				CondensedVector<int> cv(v, is_non_empty);
+				cv.Delete(testcase.size() - 5, 5);
+				v.erase(v.end() - 5, v.end());
+				Assert::IsTrue(v == cv.ToVector(v.size()), message_for_testcase(std::wstring(L"Delete from the end error."), testcase , v, cv.ToVector(v.size())).c_str());
+			}
 		}
 	};
 }
